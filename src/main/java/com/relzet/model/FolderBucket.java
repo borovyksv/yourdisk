60716dc68957a01b61268f758788d5f58ce2ad03
relzet <relzet69@gmail.com>
2016-09-25 18:15:58 +0300

    improved Security

---
+++ src/main/java/com/relzet/model/FolderBucket.java
@@ -0,0 +1,14 @@
+package com.relzet.model;
+
+public class FolderBucket {
+
+	String folderName;
+
+	public String getFolderName() {
+		return folderName;
+	}
+
+	public void setFolderName(String folderName) {
+		this.folderName = folderName;
+	}
+}
